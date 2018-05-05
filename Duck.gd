b4dbc74f14592f5a503bd6335ce98794b1d83f84
cpoll <cristian@cristianpoll.com>
2018-05-05 19:09:40 -0400

    add duck sprite

--- Duck.gd
+++ Duck.gd
@@ -6,19 +6,23 @@ var next = null
 
 
 var state
-var spawning_timer
+var scoring_timer
 var spawn_destination
+
+var SPEED
 
 var DEFAULT_COLLISION_MASK = 15
 var DEFAULT_COLLISION_LAYER = 15
 
 enum STATE {
     Playing,
+    Scoring,
     MoveToRespawn,
     Spawning
 }
 
 func _ready():
+    SPEED = 250
     respawn()
 
 func is_duck():
@@ -30,6 +34,8 @@ func _process(delta):
         playing(delta)
     elif state == STATE.Spawning:
         spawning(delta)
+    elif state == STATE.Scoring:
+        scoring(delta)
 
 
 func _integrate_forces(f_state):
@@ -48,14 +54,23 @@ func _integrate_forces(f_state):
 ###
 
 func playing(delta):
-    pass
+    var impulse_vector = Vector2(1, 0).normalized()
+
+    var max_speed = 250
+    if impulse_vector.length() > 0:
+        impulse_vector = impulse_vector.normalized() * SPEED
+
+    #else:
+    #    impulse_vector = - linear_velocity.normalized() * SPEED
+
+    apply_impulse(Vector2(), impulse_vector * delta)
+
+    if get_linear_velocity().length() > max_speed + 10:
+        var new_speed = get_linear_velocity().normalized()
+        new_speed *= max_speed
+        set_linear_velocity(new_speed)
 
 func spawning(delta):
-    spawning_timer -= delta
-
-    #if spawning_timer > 0:
-    #    print(spawning_timer)
-    #    apply_impulse(Vector2(), Vector2(0.7, 1).normalized() * 10000 * delta)
     if self.position.x < spawn_destination.x - 300:
         var vector = Vector2(1, 0.05).normalized()
         self.apply_impulse(Vector2(), vector * 20000 * delta)
@@ -66,6 +81,14 @@ func spawning(delta):
     else:
         self.linear_velocity = Vector2(0,0)
         enter_playing_state()
+        
+func scoring(delta):
+    scoring_timer -= delta
+
+    apply_impulse(Vector2(), Vector2(0, 1).normalized() * 4500 * delta)
+
+    if scoring_timer < 0:
+        self.queue_free()
 
 ###
 # State Transitions
@@ -75,7 +98,6 @@ func respawn():
     self.set_collision_mask(0)
     self.set_collision_layer(0)
     self.linear_velocity = Vector2(0,0)
-    spawning_timer = 1
     state = STATE.MoveToRespawn
     spawn_destination = Vector2(rand_range(740, 1280), rand_range(320, 1100))
 
@@ -89,6 +111,13 @@ func set_collision_stuff(mask, layer):
     self.set_collision_layer(layer)
     if self.child:
         self.child.set_collision_stuff(mask, layer)
+
+func entered_score_zone():
+    # TODO: IF UNOWNED
+    self.set_collision_mask(0)
+    self.set_collision_layer(0)
+    scoring_timer = 3.0
+    state = STATE.Scoring
 
 func _on_Duck_body_entered(body):
     pass
