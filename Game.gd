301b0ca395c38219b663cabb41823f8dd2d4fd09
Aiden Storey <aiden.derooy@gmail.com>
2018-05-04 23:57:54 -0400

    Added game loop

--- Game.gd
+++ Game.gd
@@ -4,8 +4,9 @@ extends Node
 var game_handle_time = null
 var time_remaining = 0.0
 
+var MAX_PLAYERS = 4
 var MAX_TIME_PRE_GAME = 5.0
-var MAX_TIME_GAME = 100.0
+var MAX_TIME_GAME = 10.0
 
 
 var IDLE = 'idle'
@@ -13,7 +14,30 @@ var PRE_GAME = 'pre_game'
 var GAME = 'game'
 
 var state = null
-var number_of_players = 0
+var number_of_players = 2
+
+
+func pre_game():
+    $HUD.pre_game(number_of_players, MAX_TIME_PRE_GAME)
+    update_state(PRE_GAME)
+
+
+func start_game():
+    $HUD.start_game( MAX_TIME_GAME)
+    update_state(GAME)
+
+
+func end_game():
+    $HUD.end_game()
+    update_state(IDLE)
+
+
+func update_hud():
+    if state == PRE_GAME:
+        $HUD.update_time_pre_game(time_remaining)
+    elif state == GAME:
+        $HUD.update_time_game(time_remaining)
+
 
 func update_state(_state):
     if _state == IDLE:
@@ -21,34 +45,30 @@ func update_state(_state):
     elif _state == PRE_GAME:
         time_remaining = MAX_TIME_PRE_GAME
     elif _state == GAME:
-        time_remaining = MAX_GAME
+        time_remaining = MAX_TIME_GAME
         
     state = _state
 
-func handle_transition():
-    if state == PRE_GAME:
-        start_game()
-    elif state == GAME:
-        end_game()
 
-
-func start_game():
-    $HUD.start_game(number_of_players, MAX_TIME_GAME)
-    state = GAME
-
-
-func end_game():
-    $HUD.end
-    state = IDLE
-
-
-func _ready():
-    state = IDLE
-
-
-func _process(delta):
+func update_time(delta):
     if time_remaining > 0.0:
         time_remaining = max(0.0, time_remaining - delta)
         
-        if time_temaining == 0.0:
-            handle_transition()
+        if time_remaining == 0.0:
+            if state == PRE_GAME:
+                start_game()
+            elif state == GAME:
+                end_game()
+    
+
+func _ready():
+    update_state(IDLE)
+
+
+func _process(delta):
+    update_time(delta)
+    update_hud()
+    
+    # TODO: REMOVE THIS AT SOME POINT, OR DON'T I'M NOT THE BOSS OF YOU.
+    if state == IDLE && Input.is_action_pressed('ui_up'):
+        pre_game()
--- HUD.gd
+++ HUD.gd
@@ -8,27 +8,34 @@ func update_player_score(player, score):
     player_labels[player].text = str(score)
 
 
-func update_time_remaining(time_remaining):
-    $LabelTimeRemaining.text = str(time_remaining)
+func update_time_game(time_remaining):
+    $LabelTimeGame.text = str(floor(time_remaining))
 
 
-func update_time_start(time_start):
-    $LabelTimeStart.text = str(time_start)
+func update_time_pre_game(time_remaining):
+    $LabelTimePreGame.text = str(ceil(time_remaining))
+    
 
-
-func start_game(number_of_players, time_remaining):
-    for player in range(MAX_PLAYERS):
+func pre_game(number_of_players, time_remaining):
+    for player in range(1, MAX_PLAYERS + 1):
         update_player_score(player, 0)
-        if i < number_of_players:
+        if player <= number_of_players:
             player_labels[player].show()
         else: 
             player_labels[player].hide()
     
-    update_time_remaining(time_remaining)
+    $LabelTimePreGame.show()
+    update_time_pre_game(time_remaining)
+
+
+func start_game(time_remaining):
+    $LabelTimePreGame.hide()
+    $LabelTimeGame.show()
+    update_time_game(time_remaining)
 
 
 func end_game():
-    $LabelTimeRemaining.hide()
+    $LabelTimeGame.hide()
 
 
 func _ready():
@@ -39,7 +46,9 @@ func _ready():
         4: $LabelPlayer4,
     }
     
-    
     for label in player_labels.values():
         label.text = "0"
         label.hide()
+        
+        $LabelTimeGame.hide()
+        $LabelTimePreGame.hide()
