0f7d6fad803dd39b76a7d672c51825a0c30fc637
Aiden Storey <aiden.derooy@gmail.com>
2018-05-05 10:39:40 -0400

    Changed state to enum

--- Game.gd
+++ Game.gd
@@ -8,6 +8,11 @@ var MAX_PLAYERS = 4
 var MAX_TIME_PRE_GAME = 5.0
 var MAX_TIME_GAME = 10.0
 
+enum STATE {
+    idle,
+    pre_game,
+    game,
+}
 
 var IDLE = 'idle'
 var PRE_GAME = 'pre_game'
@@ -19,32 +24,32 @@ var number_of_active_players = 0
 
 func pre_game():
     $HUD.pre_game(number_of_active_players, MAX_TIME_PRE_GAME)
-    update_state(PRE_GAME)
+    update_state(STATE.pre_game)
 
 
 func start_game():
     $HUD.start_game( MAX_TIME_GAME)
-    update_state(GAME)
+    update_state(STATE.game)
 
 
 func end_game():
     $HUD.end_game()
-    update_state(IDLE)
+    update_state(STATE.idle)
 
 
 func update_hud():
-    if state == PRE_GAME:
+    if state == STATE.pre_game:
         $HUD.update_time_pre_game(time_remaining)
-    elif state == GAME:
+    elif state == STATE.game:
         $HUD.update_time_game(time_remaining)
 
 
 func update_state(_state):
-    if _state == IDLE:
+    if _state == STATE.idle:
         pass
-    elif _state == PRE_GAME:
+    elif _state == STATE.pre_game:
         time_remaining = MAX_TIME_PRE_GAME
-    elif _state == GAME:
+    elif _state == STATE.game:
         time_remaining = MAX_TIME_GAME
         
     state = _state
@@ -55,14 +60,14 @@ func update_time(delta):
         time_remaining = max(0.0, time_remaining - delta)
         
         if time_remaining == 0.0:
-            if state == PRE_GAME:
+            if state == STATE.pre_game:
                 start_game()
-            elif state == GAME:
+            elif state == STATE.game:
                 end_game()
     
 
 func _ready():
-    update_state(IDLE)
+    update_state(STATE.idle)
 
 
 func _process(delta):
@@ -70,7 +75,7 @@ func _process(delta):
     update_hud()
     
     # TODO: REMOVE THIS AT SOME POINT, OR DON'T I'M NOT THE BOSS OF YOU.
-    if state == IDLE && Input.is_action_pressed('ui_up'):
+    if state == STATE.idle && Input.is_action_pressed('ui_up'):
         pre_game()
 
 
