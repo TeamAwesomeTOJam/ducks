79ef15c4f3834cab193866482f719f19647815bb
Aiden Storey <aiden.derooy@gmail.com>
2018-05-04 22:13:44 -0400

    inb4 merge conflicts

--- Game.gd
+++ Game.gd
@@ -1,17 +1,54 @@
 extends Node
 
 
+var game_handle_time = null
+var time_remaining = 0.0
+
+var MAX_TIME_PRE_GAME = 5.0
+var MAX_TIME_GAME = 100.0
+
+
+var IDLE = 'idle'
+var PRE_GAME = 'pre_game'
+var GAME = 'game'
+
+var state = null
+var number_of_players = 0
+
+func update_state(_state):
+    if _state == IDLE:
+        pass
+    elif _state == PRE_GAME:
+        time_remaining = MAX_TIME_PRE_GAME
+    elif _state == GAME:
+        time_remaining = MAX_GAME
+        
+    state = _state
+
+func handle_transition():
+    if state == PRE_GAME:
+        start_game()
+    elif state == GAME:
+        end_game()
+
+
 func start_game():
-	pass
+    $HUD.start_game(number_of_players, MAX_TIME_GAME)
+    state = GAME
 
 
 func end_game():
-	pass
+    $HUD.end
+    state = IDLE
 
 
 func _ready():
-	pass
+    state = IDLE
 
 
 func _process(delta):
-	pass
+    if time_remaining > 0.0:
+        time_remaining = max(0.0, time_remaining - delta)
+        
+        if time_temaining == 0.0:
+            handle_transition()
--- HUD.gd
+++ HUD.gd
@@ -5,42 +5,41 @@ var player_labels = {}
 
 
 func update_player_score(player, score):
-	player_labels[player].text = str(score)
+    player_labels[player].text = str(score)
 
 
 func update_time_remaining(time_remaining):
-	$LabelTimeRemaining.text = str(time_remaining)
-	
+    $LabelTimeRemaining.text = str(time_remaining)
+
 
 func update_time_start(time_start):
-	$LabelTimeStart.text = str(time_start)
-	
-	
-func start_game(number_of_players, time_remaining, time_start):
-	for player in range(MAX_PLAYERS):
-		update_player_score(player, 0)
-		if i < number_of_players:
-			player_labels[player].show()
-		else: 
-			player_labels[player].hide()
-		
-	update_time_remaining(time_remaining)
-	update_time_start(time_start)
+    $LabelTimeStart.text = str(time_start)
+
+
+func start_game(number_of_players, time_remaining):
+    for player in range(MAX_PLAYERS):
+        update_player_score(player, 0)
+        if i < number_of_players:
+            player_labels[player].show()
+        else: 
+            player_labels[player].hide()
+    
+    update_time_remaining(time_remaining)
 
 
 func end_game():
-	$LabelTimeRemainingg.hide()
+    $LabelTimeRemaining.hide()
 
 
 func _ready():
-	player_labels = {
-		1: $LabelPlayer1,
-		2: $LabelPlayer2,
-		3: $LabelPlayer3,
-		4: $LabelPlayer4,
-	}
-		
-		
-	for label in player_labels.values():
-		label.text = "0"
-		label.hide()
+    player_labels = {
+        1: $LabelPlayer1,
+        2: $LabelPlayer2,
+        3: $LabelPlayer3,
+        4: $LabelPlayer4,
+    }
+    
+    
+    for label in player_labels.values():
+        label.text = "0"
+        label.hide()
